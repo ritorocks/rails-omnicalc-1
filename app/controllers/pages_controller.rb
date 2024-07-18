2e14a56b3e06ec4350bca907a6a1ed0988fc16f8
ritorocks <ritorocks@gmail.com>
2024-07-18 00:47:09 +0000

    Added structure: view templates and routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+
+  def square
+
+  end
+  
+  def square
+
+  end
+
+  def square_root
+    render({:template => "pages_templates/sq_root"})
+  end
+
+
+  def payment
+  
+  end
+
+  def random
+  
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  get("/", {:controller => "pages", :action => "show"})
+  get("/square/new", {:controller => "pages", :action => "square"})
+  get("/square_root/new", {:controller => "pages", :action => "square_root"})
+  get("/payment/new", {:controller => "pages", :action => "payment"})
+  get("/random/new", {:controller => "pages", :action => "random"})
+  get("/square/results", {:controller => "pages", :action => "square_results"})
+  get("/square_root/results", {:controller => "pages", :action => "square_root_results"})
+  get("/payment/results", {:controller => "pages", :action => "payment_results"})
+  get("/random/results", {:controller => "pages", :action => "random_results"})
 end
